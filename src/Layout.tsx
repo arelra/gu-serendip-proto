9f6f50f092ccb57d7fada2f12dc34af867e0d945
Ravi Lal <arelra@users.noreply.github.com>
2021-04-22 22:10:19 +0100

    basic layout

---
+++ src/Layout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const layoutStyle = {
+  display: "grid",
+  gridTemplateColumns: "minmax(max-content, 10%) auto",
+  width: "100%",
+  padding: "0",
+  gridGap: "2rem",
+} as React.CSSProperties;
+
+interface Props {
+  children?: React.ReactNode,
+  style?: React.CSSProperties,
+}
+
+const Layout = ({ children, style}: Props) => (
+  <div style={{...layoutStyle, ...style}}>
+    {children}
+  </div>
+);
+
+export default Layout;
---
+++ src/Logo.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const style = {
+  backgroundColor: "#052962",
+  color: "white",
+  fontSize: "6rem",
+  lineHeight: "6rem",
+  borderRadius: "1rem",
+  display: "flex",
+  margin: "auto",
+  padding: "1rem",
+  paddingBottom: "3rem",
+  fontWeight: 900
+} as React.CSSProperties;
+
+const Logo = () => {
+  return <div style={style}>g</div>;
+};
+
+export { Logo };
